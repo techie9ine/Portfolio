dc2294b470461046337b73806cbfbc033630093d
emmanuel <eokpala999@gmail.com>
2024-08-03 15:05:13 +0100

    updated allowed host

--- my_portfolio_prj1/settings.py
+++ my_portfolio_prj1/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["portfolio-5ivd.onrender.com", "127.0.0.1"]
 
 
 # Application definition
